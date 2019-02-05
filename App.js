44102292b6784d76401c995a9f8842366f732645
Jacky <32426170+jacquelinesaelee@users.noreply.github.com>
2019-02-04 22:21:09 -0800

    React Navigation V3
    - Navigation is updated to version 3
    - try:
         npm install -g expo-cli
         npm update -g react-navigation

--- App.js
+++ App.js
@@ -2,21 +2,36 @@
  Navigation Page
 */
 import React from 'react';
-import { StackNavigator} from 'react-navigation';
+import {RkButton} from 'react-native-ui-kitten';
+// V3 NAV ERROR
+//import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer} from 'react-navigation';
+
 import Main from './Screens/Main';
 import HomeScreen from './Screens/HomeScreen';
 import Signup from './Screens/Signup';
+import SignupVendor from './Screens/SignupVendor';
+import HomeScreenVendor from './Screens/HomeScreenVendor';
 
+
+// V3 NAV ERROR
+// export default ...
 export default class App extends React.Component{
     render(){
         return(
-            <AppNavigator/>  
+            // V3 NAV ERROR
+            <AppContainer/>  
+            
+            
         );
      }
 }
-
-const AppNavigator = StackNavigator({
+const AppStackNavigator = createStackNavigator({
     MainScreen: {screen: Main},
     HomeScreen: {screen: HomeScreen},
     SignupScreen: {screen: Signup},
+    SignupVendor: {screen: SignupVendor},
+    HomeScreenVendor: {screen: HomeScreenVendor}
 });
+
+const AppContainer = createAppContainer(AppStackNavigator);
