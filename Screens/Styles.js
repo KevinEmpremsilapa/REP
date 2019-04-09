7758e811a583fe513f5e61230a816049fb49bc58
Jacky <32426170+jacquelinesaelee@users.noreply.github.com>
2019-04-08 19:07:40 -0700

    Updated Vendor Home Page
    - Map fills screen
    - Turn ON/OFF location button
    - Go to current location button
    - Hamburger menu button

--- Screens/Styles.js
+++ Screens/Styles.js
@@ -1,11 +1,54 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const Colors = {
+  lightGrey: '#c9c9c9',
+  midGrey: '#5d5d5d',
+  darkGrey: '#363636',
+}
 
 export default StyleSheet.create({
+  safeAreaStyle:{height:100,backgroundColor: 'white', alignItems:'center', justifyContent: 'center'},
+  menuImage:{height:60, width:60, borderRadius:60},
+  drawerIcon:{fontSize:24, color:'#4C2250'},
+  menuOptionsStyle: {flex:1},
+  headerLayoutStyle: { 
+    height: 100,
+    width: Dimensions.get('window').width, 
+    backgroundColor: '#FBBC82', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  slidingPanelLayoutStyle: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    backgroundColor: '#CD9BCC', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  slidingPanelLayout2Style: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    backgroundColor: 'white'
+  },
+  commonTextStyle: {
+    color: 'white', 
+    fontSize: 18
+  },
+  panel: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center'
+    },
+  container2: {
+    flex: 1,
+    backgroundColor: '#fff'
     },
   padding: {
     paddingTop: 23
@@ -70,6 +113,7 @@ export default StyleSheet.create({
     marginBottom: 20
   },
 
+
   // Style Form
   formInput:{
     marginTop: 10, 
@@ -84,6 +128,9 @@ export default StyleSheet.create({
     padding: 5, 
     textAlign: "center",
     fontWeight: 'bold'
+  },
+  searchBarContainer:{
+    backgroundColor: '#FFF',
   },
 
   // Buttons
@@ -106,7 +153,61 @@ export default StyleSheet.create({
     resizeMode: 'contain',
     height: '100%',
     width: '100%',
-  }
+  },
+  
+  // Vendor 
+  vendorThumbnail:{
+    height: 50,
+    width: 50,
+  },
+  vendorName:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.darkGrey,
+  },
+  vendorLocationButtonAlign:{
+   position: 'absolute',
+   justifyContent: 'center', 
+   alignItems: 'center',
+   alignSelf: 'center',
+   //alignSelf: 'center',
+   bottom: 20,
+  },
+  vendorMapContainer:{
+    flex:1,
+  },
+  vendorHeaderStyle:{
+    backgroundColor: 'rgba(255,109,111, .8)',
+    position: 'absolute',
+    top: 0,
+  },
+
+  // Map 
+  mapSize:{
+    flex: 1,
+  },
+  compassPosition:{
+    position: 'absolute',
+    alignSelf: 'center',
+    top: 10,
+  },
+  hamburgerIconPosition:{
+    position: 'absolute',
+    paddingRight: 20,
+    top: 60,
+    right:0,
+    paddingBottom: 10,
+  },
+  mapIconStyle:{
+    height: 50,
+    width: 50,
+  },
+  locationIconPosition:{
+    position: 'absolute',
+    paddingRight: 20,
+    top: 120,
+    right:0,
+  },
 })
 
 /*
@@ -114,20 +215,24 @@ Colors:
  // Original Image Colors (Currently in use)
   #FBBC82
   #FB8B74   
-  #FF6D6F  
+  #FF6D6F rgba(255,109,111, .8)
   #8F425F
   #87456F
   #4C2250
-
   // Faded Image Colors
   #FAB7AF
   #F7B8B1
   #CF9FC7
   #CD9BCC
-
   #F8B195
   #F67280
   #C06C84
   #6C5B7B
   #355C7D
+
+  // Font Colors
+  #363636
+
+  // Grey
+  #C7C7C7 rgba(199,199,199, .8)
  */
